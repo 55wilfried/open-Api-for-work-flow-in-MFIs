775349fbdeafc53a88822ed957ffe4a48af39e95
Wilfried55 <Mafouo181099@>
2024-06-24 17:40:34 +0100

    all modules four

--- loans-services/src/main/java/com/microfinance/loans_services/loansRepository/LoanRepository.java
+++ loans-services/src/main/java/com/microfinance/loans_services/loansRepository/LoanRepository.java
@@ -1,8 +1,11 @@
 package com.microfinance.loans_services.loansRepository;
 
+import com.microfinance.loans_services.models.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoanRepository extends JpaRepository {
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+
 }
---
+++ loans-services/src/main/java/com/microfinance/loans_services/models/Loan.java
@@ -0,0 +1,13 @@
+package com.microfinance.loans_services.models;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Loan {
+    @Id
+    private Long id;
+    // Other fields and getters/setters
+}
